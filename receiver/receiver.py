7706319274794d8317c30d14f061648e18a7ad9a
JF Delplace <33230539+jfdelplace@users.noreply.github.com>
2021-12-29 01:45:08 +0100

    Clean up runner

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -6,7 +6,6 @@ from collections import deque
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras import Input
 
 class DeepQLearnerAgent:
     """
@@ -14,33 +13,37 @@ class DeepQLearnerAgent:
     """
 
     def __init__(self,
-                 env,
                  dim_msg: int = 0,
                  n_states: int = 25, 
                  n_actions: int = 4, 
-                 learning_rate: float = 1e-4,
-                 gamma: float = 0.9,
+                 learning_rate: float = 1e-3,
+                 gamma: float = 0.95,
                  epsilon_max: Optional[float] = 1,
                  epsilon_min: Optional[float] = 0.01,
-                 epsilon_decay: Optional[float] = 0.999):
+                 epsilon_decay: Optional[float] = 0.996):
         """
+        :param dim_msg: The dimension of the sender's message.
+        :param n_states: The number of states.
+        :param n_actions: The number of actions.
         :param learning_rate: The learning rate.
         :param gamma: The discount factor.
         :param epsilon_max: The maximum epsilon of epsilon-greedy.
         :param epsilon_min: The minimum epsilon of epsilon-greedy.
         :param epsilon_decay: The decay factor of epsilon-greedy.
         """
-        self.memory  = deque(maxlen=2000)
-        self.n_cells = env.row * env.col
-        self.n_states = 25#n_states + dim_msg
+        self.n_states = n_states + dim_msg
         self.n_actions = n_actions
-        self.batch_size = 15
+
+        self.alpha = learning_rate
         self.gamma = gamma
+
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.epsilon_max = epsilon_max
         self.epsilon = epsilon_max
-        self.learning_rate = learning_rate
+
+        self.batch_size = 15
+        self.memory  = deque(maxlen=2000)
         self.model = self.create_nnet()
     
     def create_nnet(self) -> Sequential:
@@ -48,7 +51,6 @@ class DeepQLearnerAgent:
         Function that returns an nnet
         :return: nnet: ...
         """
-
         model = Sequential(
             [
                 Dense(24, input_dim=self.n_states, activation='relu', name="input"),
@@ -56,43 +58,33 @@ class DeepQLearnerAgent:
                 Dense(self.n_actions, activation='linear', name="output")
             ]    
         )
-
-        model.compile(loss='mse', optimizer=RMSprop(self.learning_rate), metrics=["accuracy", "mse"])
+        model.compile(loss='mse', optimizer=RMSprop(self.alpha), metrics=["accuracy", "mse"])
 
         return model
 
     def greedy_action(self, obs) -> int:
         """
         Return the greedy action.
-        :param observation: The observation.
+        :param obs: The observation.
         :return: The action.
         """
-        q_values = self.model.predict_step(obs)
-        action = argmax(q_values)
-        print(f"Q-values {q_values}")
-        print(f"Action: {action}")
-        return action
+        return argmax(self.model.predict_step(obs.reshape(-1,self.n_states)))
 
     def act(self, obs: ndarray, training: bool = True) -> int:
         """
         Return the action.
-        :param observation: The observation.
+        :param obs: The observation.
         :param training: Boolean flag for training, when not training agent
         should act greedily.
         :return: The action.
         """
-        obs = obs.reshape(-1,self.n_states)
-
-        if(not training):
+        epsilon_rate = uniform(0,1)
+        # Exploration-Exploitation trade-off
+        if (not training) or epsilon_rate > self.epsilon:
             return self.greedy_action(obs)
         else:
-            # Exploration-Exploitation trade-off
-            epsilon_rate = uniform(0,1)
-            if epsilon_rate < self.epsilon:
-                return randint(0,3)
-            else:
-                return self.greedy_action(obs)
-
+            return randint(0,3)  
+    
     def learn(self, obs: ndarray, act: int, rew: float, done: bool, next_obs: ndarray) -> None:
         """
         Update the Q-Value.
@@ -104,6 +96,7 @@ class DeepQLearnerAgent:
         """
         self.remember(obs, act, rew, done, next_obs)
         history = self.replay()
+
         # Epsilon decay
         if(done):
             self.epsilon = max(self.epsilon*self.epsilon_decay, self.epsilon_min)
@@ -111,26 +104,9 @@ class DeepQLearnerAgent:
         return history
 
     def remember(self, obs: ndarray, act: int, rew: float, done: bool, next_obs: ndarray):
-        self.memory.append([obs.reshape(-1,self.n_states), act, rew, done, next_obs.reshape(-1,self.n_states)])
         self.memory.append([obs, act, rew, done, next_obs])
 
-    def replay_old(self):
-        batch_size = 10
-        if len(self.memory) < batch_size: 
-            return
-        samples = rsample(self.memory, batch_size)
-        for sample in samples:
-            state, action, reward, done, new_state = sample
-            target = self.model.predict(state)
-            if done:
-                target[0][action] = reward
-            else:
-                Q_future = max(self.model.predict(new_state)[0])
-                target[0][action] = reward + Q_future * self.gamma
-            return self.model.fit(state, target, epochs=1, verbose=0)
-
     def replay(self):
-    
         if len(self.memory) < self.batch_size: 
             return
 
@@ -144,14 +120,12 @@ class DeepQLearnerAgent:
         # Get future states from minibatch, then query NN model for Q values
         # When using target network, query it, otherwise main network should be queried
         new_current_states = array([transition[4] for transition in minibatch])
-        # future_qs_list = self.target_model.predict(new_current_states)
         future_qs_list = self.model.predict(new_current_states)
 
-        X = []
-        y = []
+        X, y = [], []
 
         # Now we need to enumerate our batches
-        for index, (current_state, action, reward, done, new_current_state) in enumerate(minibatch):
+        for index, (current_state, action, reward, done, _) in enumerate(minibatch):
 
             # If not a terminal state, get new q from future states, otherwise set it to 0
             # almost like with Q Learning, but we use just part of equation here
@@ -161,6 +135,7 @@ class DeepQLearnerAgent:
             else:
                 new_q = reward
 
+            # Update Q value for given state
             current_qs = current_qs_list[index]
             current_qs[action] = new_q
 
@@ -170,6 +145,18 @@ class DeepQLearnerAgent:
 
         # Fit on all samples as one batch, log only on terminal state
         self.model.fit(array(X), array(y), batch_size=self.batch_size, verbose=0, shuffle=False)
-        # print(self.model.summary())
-        # print(self.model.weights)
-        # input("press enter to continue...")
+
+    # def replay_simple(self):
+    #     batch_size = 10
+    #     if len(self.memory) < batch_size: 
+    #         return
+    #     samples = rsample(self.memory, batch_size)
+    #     for sample in samples:
+    #         state, action, reward, done, new_state = sample
+    #         target = self.model.predict(state)
+    #         if done:
+    #             target[0][action] = reward
+    #         else:
+    #             Q_future = max(self.model.predict(new_state)[0])
+    #             target[0][action] = reward + Q_future * self.gamma
+    #         return self.model.fit(state, target, epochs=1, verbose=0)
