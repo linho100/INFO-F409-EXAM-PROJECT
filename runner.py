1424a3b2ce96c79e20d4b1b154959671f397ee16
JF Delplace <33230539+jfdelplace@users.noreply.github.com>
2022-01-08 11:00:47 +0100

    Cleanup runner.py

--- runner.py
+++ runner.py
@@ -17,19 +17,20 @@ from agents.q_learner_agent import QLearnerAgent
 def training_episode(env: GridWorld, receiver: DeepQLearnerAgent, senders_list: Iterable[SenderAgent],
                      layout_type: int):
     """
-    Interact with the environment for one episode using actions derived from the nnet.
+    Interacts with the environment for one episode using actions provided by the models predictions in order to train said models.
     :param env: The environment.
     :param receiver: The receiver agent.
-    :param senders_list:
-    :param training: If true the q_table will be updated using q-learning. The flag is also passed to the action selector.
-    :param layout_type:
-    :return: The average cumulated reward.
+    :param senders_list: The list of senders agents.
+    :param layout_type: Int id of the env's layout to be used.
+    :return: The number of steps performed.
     """
     done = False
     obs = env.reset(layout_type)
     context_vector = env.one_hot_enc_goal()
     step = 0
 
+    # For each sender, retrieve the message corresponding to the goal position
+    # Then concatenate all the messages into a single one
     if senders_list:
         messages_encoded = [-1]
         senders_nb = len(senders_list)
@@ -37,24 +38,25 @@ def training_episode(env: GridWorld, receiver: DeepQLearnerAgent, senders_list: 
         for i in range(senders_nb):
             senders_list[i].goal_location = context_vector
             message = senders_list[i].send_message()
-            messages_encoded = concatenate(
-                (messages_encoded, message))  # Store the messages sent
+            messages_encoded = concatenate((messages_encoded, message))
             obs = concatenate((obs, message))
 
         messages_encoded = delete(messages_encoded, 0)
 
+    # Run the episode
     while not done:
         action = receiver.act(obs, True)
         obs_prime, reward, done = env.step(action)
         if senders_list:
             obs_prime = concatenate((obs_prime, messages_encoded))
+        # Receiver earning
         receiver.learn(obs, action, reward, done, obs_prime)
 
         obs = obs_prime
         step += 1
 
+    # Senders learning
     if senders_list:
-        # Senders learning
         for i in range(senders_nb):
             senders_list[i].learn(reward)
 
@@ -64,13 +66,12 @@ def training_episode(env: GridWorld, receiver: DeepQLearnerAgent, senders_list: 
 def evaluation_episode(env: GridWorld, receiver: DeepQLearnerAgent, senders_list: Iterable[SenderAgent],
                        layout_type: int):
     """
-    Interact with the environment for one episode using actions derived from the nnet.
+    Interacts with the environment for one episode using actions provided by the models predictions in order to evaluate said models.
     :param env: The environment.
     :param receiver: The receiver agent.
-    :param senders_list:
-    :param training: If true the q_table will be updated using q-learning. The flag is also passed to the action selector.
-    :param layout_type:
-    :return: The average cumulated reward.
+    :param senders_list: The list of senders agents.
+    :param layout_type: Int id of the env's layout to be used.
+    :return: The reward received at the end of the episode divided by the number of steps performed.
     """
     done = False
     obs = env.reset(layout_type)
@@ -100,28 +101,35 @@ def evaluation_episode(env: GridWorld, receiver: DeepQLearnerAgent, senders_list
 
 
 def exp_0_random(layout=0):
+    """
+    Runs a random agent for the same number of steps as for the subsequent agents to be used as a reference point.
+    :param layout: Int id of the env's layout to be used.
+    """
     # Hyperparameters
     gamma = 0.9
 
+    # Env and agent
     env = GridWorld(p_term=1 - gamma)
-    receiver = RandomAgent()
+    agent = RandomAgent()
 
-    # Training
+    # Run configuration
     steps_to_backup = 50000
     last_backup = 0
-
-    # Testing episodes : 20 episodes every 2000 steps
-    step = 1
     max_step = 600000
     last_evaluation = 0
     steps_to_evaluation = 2000
     number_episode_per_evaluation = 20
+
     global_results = []
+
+    step = 1
     start_t = time()
     while step < max_step:
+        # No training episode required
         step += 1
 
         if step >= last_evaluation + steps_to_evaluation:
+            # Evaluation episodes
             last_evaluation = step
             evaluation_reward = 0
             for _ in range(number_episode_per_evaluation):
@@ -130,7 +138,7 @@ def exp_0_random(layout=0):
                 ev_step = 0
 
                 while not done:
-                    _, reward, done = env.step(receiver.act())
+                    _, reward, done = env.step(agent.act())
                     ev_step += 1
 
                 evaluation_reward += reward / ev_step
@@ -155,30 +163,36 @@ def exp_0_random(layout=0):
 
 
 def exp_0_q_learner(layout=0):
+    """
+    Runs a basic q-learner agent for the same number of steps as for the subsequent agents to be used as a reference point.
+    Evaluation is performed periodically during training to follow its progression.
+    :param layout: Int id of the env's layout to be used.
+    """
+
     # Hyperparameters
     gamma = 0.9
     n_states = 25
 
+    # Env and agent
     env = GridWorld(p_term=1 - gamma)
-    receiver = QLearnerAgent(
-        num_states=n_states, gamma=gamma)
+    agent = QLearnerAgent(num_states=n_states, gamma=gamma)
 
-    # Training
-    steps_to_backup = 50000
+    # Run configuration
     last_backup = 0
-
-    # Testing episodes : 20 episodes every 2000 steps
-    step = 1
+    steps_to_backup = 50000
     max_step = 600000
     last_evaluation = 0
     steps_to_evaluation = 2000
     number_episode_per_evaluation = 20
+
     global_results = []
+
     start_t = time()
+    step = 1
     while step < max_step:
-        # Train
+        # Training episode
         episode_step = training_episode(
-            env, receiver, senders_list=None, layout_type=layout)
+            env, agent, senders_list=None, layout_type=layout)
         step += episode_step
 
         # Evaluate and feedback
@@ -187,7 +201,7 @@ def exp_0_q_learner(layout=0):
             evaluation_reward = 0
             for _ in range(number_episode_per_evaluation):
                 evaluation_reward += evaluation_episode(
-                    env, receiver, senders_list=None, layout_type=layout)
+                    env, agent, senders_list=None, layout_type=layout)
             global_results.append(
                 [step, evaluation_reward / number_episode_per_evaluation])
 
@@ -208,6 +222,12 @@ def exp_0_q_learner(layout=0):
 
 
 def exp_1(layout=0, senders_nb=1):
+    """
+    Trains senders agents and one receiver DeepQL agent on a given grid layout.
+    Evaluation is performed periodically during training to follow its progression.
+    :param layout: Int id of the env's layout to be used.
+    :param senders_nb: Number of senders agent to be trained.
+    """
     # Hyperparameters
     gamma = 0.9
     n_states = 25
@@ -218,25 +238,25 @@ def exp_1(layout=0, senders_nb=1):
     epsilon_r = 0.005
     epsilon_s = 0.005
 
-    # Create the model
+    # Create the env and the models
     env = GridWorld(p_term=1 - gamma)
     senders = [SenderAgent(messages_nb=messages_nb, epsilon=epsilon_s)
                for _ in range(senders_nb)]
     receiver = DeepQLearnerAgent(dim_msg=dim_msg, n_states=n_states, n_senders=senders_nb, epsilon_decay=1 - epsilon_r,
                                  gamma=gamma)
 
-    # Training
+    # Run configuration
     steps_to_backup = 50000
     last_backup = 0
-
-    # Testing episodes : 20 episodes every 2000 steps
-    step = 1
     max_step = 600000
     last_evaluation = 0
     steps_to_evaluation = 2000
     number_episode_per_evaluation = 20
+
     global_results = []
+
     start_t = time()
+    step = 1
     while step < max_step:
         # Train
         episode_step = training_episode(
@@ -269,7 +289,14 @@ def exp_1(layout=0, senders_nb=1):
             print("Backup is done!")
 
 
-def exp_2(channel_capacity, layout):  # Channel capacity
+def exp_2(channel_capacity, layout):
+    """
+    Trains 1 sender agents and one receiver DeepQL agent on a given grid layout with a choosen communication channel capacity.
+    Evaluation is performed only at the end of training
+    :param layout: Int id of the env's layout to be used.
+    :param channel_capacity: Capacity of the communication channel between both agents.
+    """
+
     # Hyperparameters
     gamma = 0.9
     n_states = 25
@@ -280,20 +307,21 @@ def exp_2(channel_capacity, layout):  # Channel capacity
     epsilon_r = 0.005
     epsilon_s = 0.005
 
-    # Create the model
+    # Create the env and the models
     env = GridWorld(p_term=1 - gamma)
     senders = [SenderAgent(messages_nb=messages_nb, epsilon=epsilon_s)
                for _ in range(senders_nb)]
     receiver = DeepQLearnerAgent(dim_msg=dim_msg, n_states=n_states, n_senders=senders_nb, epsilon_decay=1 - epsilon_r,
                                  gamma=gamma)
 
-    # Train
+    # Run configuration
     step = 1
     max_step = 250000
     start_t = time()
     steps_to_feedback = 2000
     last_feedback = 0
     while step < max_step:
+        # Training episode
         episode_step = training_episode(
             env, receiver, senders, layout_type=layout)
         step += episode_step
@@ -325,6 +353,11 @@ def exp_2(channel_capacity, layout):  # Channel capacity
 
 
 def convert_to_time(seconds):
+    """
+    Basic helper to convert seconds into human-readable format.
+    :param seconds: Number of seconds to convert.
+    :return: Time following the 'd day(s) - hh:mm:ss' format.
+    """
     seconds = max(seconds, 0)
     days = seconds // (24 * 3600)
     seconds = seconds % (24 * 3600)
@@ -337,6 +370,15 @@ def convert_to_time(seconds):
 
 
 def save_results(results, r_model, s_models, experiment_number, layout, subtitle):
+    """
+    Saves the models and the training results in .csv files.
+    :param results: Array containing the results of the evaluation of the models.
+    :param r_model: Receiver model.
+    :param s_models: Senders models.
+    :param experiment_number: Int.
+    :param layout: Int id of the env's layout used.
+    :param subtitle: str.
+    """
     # Create directories
     folder_name = f"experiment_{experiment_number}_layout_{layout}"
     create_directories(folder_name)
@@ -372,6 +414,10 @@ def save_results(results, r_model, s_models, experiment_number, layout, subtitle
 
 
 def create_directories(folder_name):
+    """
+    Helper to create all the necessary directories if they are missing
+    :param folder_name: Experiment base folder
+    """
     try:
         mkdir(f"./experiments/")
     except FileExistsError:
@@ -395,19 +441,19 @@ def create_directories(folder_name):
 
 
 if __name__ == '__main__':
-    # Type 0 experiments (Random and Q-learner agents)
+    # Random and Q-learner agents "training"
     layouts = [0, 2, 4]
     for l in layouts:
         exp_0_random(l)
         exp_0_q_learner(l)
 
-    # Type 1 experiments
+    # Extended training of various configurations of multi-senders agents with 1 DeepQL receiver agent
     senders_nb = [1, 3]
     for l in layouts:
         for n in senders_nb:
             exp_1(layout=l, senders_nb=n)
 
-    # Type 2 experiments
+    # Training of single sender with single DeepQL receiver using different channel capacities
     channel_capacities = [3, 4, 5, 8, 9, 16]
     layouts = [1, 3]
 
